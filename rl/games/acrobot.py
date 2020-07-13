9bf76ed9853de9c869f4c0157278c65e958549e2
John Hu <im@john.hu>
2020-07-13 18:06:43 +0800

    add more info about a game

--- rl/games/acrobot.py
+++ rl/games/acrobot.py
@@ -1,6 +1,21 @@
 from .total_reward_gym_game import TotalRewardGymGame
 
 
+# Input:
+#     * 0: apply -1 torque on the joint of 2 pendulum links
+#     * 1: apply 0 torque on the joint of 2 pendulum links
+#     * 2: apply +1 torque on the joint of 2 pendulum links
+# Observe:
+#     * 0: 1st pendulum angle (-pi to pi)
+#     * 1: 2nd pendulum angle (-pi to pi)
+#     * 2: 1st pendulum velocity (-4 pi to 4 pi)
+#     * 3: 2nd pendulum velocity (-9 pi to 9 pi)
+# Rewards:
+#     * 0: -cos(s[0]) - cos(s[1] + s[0]) > 1
+#     * -1: otherwise
+# Termination:
+#     * -cos(s[0]) - cos(s[1] + s[0]) > 1.
+#     * step == 500
 class Acrobot(TotalRewardGymGame):
     @property
     def game_name(self):
--- rl/games/cartpole.py
+++ rl/games/cartpole.py
@@ -1,6 +1,21 @@
 from .gym_base import GymBaseGame
 
 
+# Input:
+#     * 0: push left
+#     * 1: push right
+# Observe:
+#     * 0: Cart position (-4.8 ~ 4.8)
+#     * 1: Cart velocity (-Inf ~ Inf)
+#     * 2: Pole angle (-0.418 radius, -24deg ~ 0.418 radius 24 deg)
+#     * 3: Pole angular velocity (-Inf ~ Inf)
+# Rewards:
+#     * 0: terminated
+#     * 1: still alive
+# Termination:
+#     * Pole angle > 12 deg or Pole angle < -12 deg
+#     * Cart position > 2.4 or Cart position < -2.4
+#     * step == 500
 class CartPole(GymBaseGame):
     def __init__(self, cfg, agent_cls, model_cls):
         super().__init__(cfg, agent_cls, model_cls)
