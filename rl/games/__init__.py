bb6a82af2403ca66554fdd6f1b8328c3a8586633
John Hu <im@john.hu>
2020-07-13 12:06:04 +0800

    add mountain car

--- rl/games/__init__.py
+++ rl/games/__init__.py
@@ -1,5 +1,6 @@
 from .acrobot import Acrobot
 from .cartpole import CartPole
+from .mountain_car import MountainCar
 
 
 def create_game(game, cfg, agent_cls, model_cls):
@@ -7,4 +8,6 @@ def create_game(game, cfg, agent_cls, model_cls):
         return Acrobot(cfg, agent_cls, model_cls)
     if game == 'cartpole':
         return CartPole(cfg, agent_cls, model_cls)
+    if game == "mountain_car":
+        return MountainCar(cfg, agent_cls, model_cls)
     raise f'game {game} is not supported'
